0ddbe5a249a86c6ee08c45cc2b663ab5c14f690c
Henrique David de Medeiros <henriquemed101@gmail.com>
2019-09-07 09:38:48 -0300

    Ajustes para ganho de desempenho

--- multiplicacao_paralela.py
+++ multiplicacao_paralela.py
@@ -9,22 +9,23 @@ def add(val1, val2, valor):
     return valor
 
 
-def calculaLineByCol(matrizA, matrizB, matrizResult):
-        self_t = threading.currentThread()
-        print(self_t.ident)
+def calculaLineByCol(matrizA, matrizB, matrizResult, i_posic):
         for e in range(0, len(matrizB[0])):
                 soma = 0
                 for k in range(0, len(matrizA[0])):
-                    soma += add(matrizA[i][k], matrizB[k][e], soma)
-                matrizResult[i][e] = soma
+                    soma += add(matrizA[i_posic][k], matrizB[k][e], soma)
+                matrizResult[i_posic][e] = soma
 
 
-matrizA = [[3, 0, 2],
-           [9, 1, 7],
-           [1, 0, 1]]
-matrizB = [[1, 0, -2],
-           [-2, 1, -3],
-           [-1, 0, 3]]
+#matrizA = [[3, 0, 2],
+#           [9, 1, 7],
+#           [1, 0, 1]]
+#matrizB = [[1, 0, -2],
+#           [-2, 1, -3],
+#           [-1, 0, 3]]
+
+matrizA = [[2,1,4], [0,1,1]]
+matrizB = [[6,3,-1,0], [1,1,0,4], [-2,5,0,2]]
 
 #matrizResult = [[9, 9, 9], [9, 9, 9], [9, 9, 9]]
 matrizResult = []
@@ -41,11 +42,11 @@ t = time.time()
 
 for i in range(0, len(matrizA)):
     # para cada linha da matriz chama uma thread para executar a linha
-    t1 = threading.Thread(target=calculaLineByCol, args=(matrizA, matrizB, matrizResult))
+    t1 = threading.Thread(target=calculaLineByCol, args=(matrizA, matrizB, matrizResult, i))
     t1.start()
     threads.append(t1)
 
-    for e in threads:
+for e in threads:
         e.join()
 
 print("Decorrido em paralelo: ", time.time()-t)
--- normal.py
+++ normal.py
@@ -1,9 +1,18 @@
 import time
 
-matrizA = [[3,0,2],[9,1,7],[1,0,1]]
-matrizB = [[1,0,-2],[-2,1,-3],[-1,0,3]]
+#matrizA = [[3,0,2],[9,1,7],[1,0,1]]
+#matrizB = [[1,0,-2],[-2,1,-3],[-1,0,3]]
+matrizA = [[2,1,4], [0,1,1]]
+matrizB = [[6,3,-1,0], [1,1,0,4], [-2,5,0,2]]
 
-matrizResult = [[9,9,9],[9,9,9],[9,9,9]]
+#matrizResult = [[9,9,9],[9,9,9],[9,9,9]]
+matrizResult = []
+
+for i in range(0, len(matrizA)):
+        tabela1 = []
+        for e in range(0, len(matrizB[0])):
+                tabela1.append(0)
+        matrizResult.append(tabela1)
 
 if len(matrizA[0]) != len(matrizB):
     print("O numero de linha da matriz A tem que ser igual a da matriz B!")
